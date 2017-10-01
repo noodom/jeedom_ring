1c1f05c4f60902937b13c9a6c6e75ef1858fc0df
lunarok@gmail.com <lunarok@gmail.com>
2017-10-01 22:41:45 +0200

    push

--- desktop/php/ring.php
+++ desktop/php/ring.php
@@ -74,7 +74,7 @@ $eqLogics = eqLogic::byType('ring');
         <form class="form-horizontal">
           <fieldset>
             <div class="form-group">
-              <label class="col-sm-3 control-label">{{Nom du The Keys}}</label>
+              <label class="col-sm-3 control-label">{{Nom du Visiophone}}</label>
               <div class="col-sm-3">
                 <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
                 <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement ring}}"/>
