62908df41ec8aa200afe529ceef13ae238e3dcbd
Cédric Dupont <lunarok@gmail.com>
2017-10-05 16:21:51 +0200

    update

--- core/class/ring.class.php
+++ core/class/ring.class.php
@@ -18,169 +18,76 @@
 
 /* * ***************************Includes********************************* */
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+if (!class_exists('ringAPI')) { require_once dirname(__FILE__) . '/../../3rdparty/ringAPI.php'; }
 
 class ring extends eqLogic {
-  private $_apiProto      = 'https://';
-  private $_apiHost       = "api.ring.com";
-  private $_apiVersion    = 9;
-  private $_urlSession    = '/clients_api/session';
-  private $_urlDings      = '/clients_api/dings/active';
-  private $_urlDevices    = '/clients_api/ring_devices';
-  private $_urlHistory    = '/clients_api/doorbots/history';
-  private $_urlRecording  = '/clients_api/dings/{id}/recording';
-  private $_authToken     = null;
-
-  private function _httpCall($method, $call, $data, $username = null, $password = null) {
-    $urlParameters = '';
-    $headers = array();
-    $headers[] = 'Accept-Encoding: gzip, deflate';
-    $headers[] = 'User-Agent: Dalvik/1.6.0 (Linux; U; Android 4.4.4; Build/KTU84Q)';
-
-
-    $ch = curl_init();
-    if ($method == 'POST') {
-      $headers[] = 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8';
-      $postBody = $this->_arrayToUrlString($data);
-      curl_setopt($ch, CURLOPT_POST, 1);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $postBody);
-    } else {
-      $urlParameters = '?'.$this->_arrayToUrlString($data);
-    }
-
-    $url = $this->_apiProto.$this->_apiHost.$call.$urlParameters;
-    print "Call: ".$url."\n";
-    curl_setopt($ch, CURLOPT_URL, $url);
-    if (isset($username) || isset($password)) {
-      curl_setopt($ch, CURLOPT_USERPWD,  $username.":".$password);
-    }
-
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_ENCODING , "gzip, deflate");
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    /*
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_VERBOSE, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 1);
-    */
-    $serverResponse = curl_exec($ch);
-    $httpCode       = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    //var_dump($serverResponse);
-    curl_close ($ch);
-
-    // Try JSON Decode
-    $json = json_decode($serverResponse);
-    if ($json) {
-      return $json;
-    } else {
-      return $serverResponse;
-    }
-  }
-
-  function authenticate($username, $password) {
-    $postData['device[os]']                             = 'android';
-    $postData['device[hardware_id]']                    = '180940d0-7285-3366-8c64-6ea91491982c';
-    $postData['device[app_brand]']                      = 'ring';
-    $postData['device[metadata][device_model]']         = 'VirtualBox';
-    $postData['device[metadata][resolution]']           = '600x800';
-    $postData['device[metadata][app_version]']          = '1.7.29';
-    $postData['device[metadata][app_instalation_date]'] = '';
-    $postData['device[metadata][os_version]']           = '4.4.4';
-    $postData['device[metadata][manufacturer]']         = 'innotek GmbH';
-    $postData['device[metadata][is_tablet]']            = 'true';
-    $postData['device[metadata][linphone_initialized]'] = 'true';
-    $postData['device[metadata][language]']             = 'en';
-    $postData['api_version']                            = $this->_apiVersion;
-    $headers = array();
-
-    $response = $this->_httpCall('POST', $this->_urlSession, $postData, $username, $password);
-
-    print "Authenticated as ".$response->profile->first_name.' '.$response->profile->last_name."\n";
-    print "Authentication token is ".$response->profile->authentication_token."\n";
-    $this->_authToken = $response->profile->authentication_token;
-  }
-
-  function poll() {
-    $result = array();
-    $data = array();
-    $data['api_version'] = $this->_apiVersion;
-    $data['auth_token']  = $this->_authToken;
-    $response = $this->_httpCall('GET', $this->_urlDings, $data);
-    foreach($response as $status) {
-      foreach($status as $k => $v) {
-        $result[$status->id][$k] = $v;
-      }
-      $result[$status->id]['is_motion']   = false;
-      $result[$status->id]['is_ding']     = false;
-      if ($status->state == 'ringing') {
-        if ($status->kind == 'motion') {
-          $result[$status->id]['is_motion'] = true;
+    public static function deamon_info() {
+        $return = array();
+        $return['log'] = '';
+        $return['state'] = 'nok';
+        $cron = cron::byClassAndFunction('ring', 'daemon');
+        if (is_object($cron) && $cron->running()) {
+            $return['state'] = 'ok';
         }
-        if ($status->kind =='ding') {
-          $result[$status->id]['is_ding'] = true;
+        $return['launchable'] = 'ok';
+        return $return;
+    }
+
+    public static function deamon_start($_debug = false) {
+        self::deamon_stop();
+        $deamon_info = self::deamon_info();
+        if ($deamon_info['launchable'] != 'ok') {
+            throw new Exception(__('Veuillez vérifier la configuration', __FILE__));
         }
-      }
+        $cron = cron::byClassAndFunction('ring', 'daemon');
+        if (!is_object($cron)) {
+            throw new Exception(__('Tache cron introuvable', __FILE__));
+        }
+        $cron->run();
     }
 
-    if ($result) {
-      return $result;
-    } else {
-      return false;
+    public static function deamon_stop() {
+        $cron = cron::byClassAndFunction('ring', 'daemon');
+        if (!is_object($cron)) {
+            throw new Exception(__('Tache cron introuvable', __FILE__));
+        }
+        $cron->halt();
     }
-  }
 
-  function history() {
-    $result = array();
-    $data = array();
-    $data['api_version'] = $this->_apiVersion;
-    $data['auth_token']  = $this->_authToken;
-    $data['limit']       = 30;
-    $response = $this->_httpCall('GET', $this->_urlHistory, $data);
-    return $response;
-  }
+    public static function daemon() {
+        $username = config::byKey('username', 'ring');
+        $password = config::byKey('password', 'ring');
+        $bell = new RingAPI();
+        log::add('ring', 'debug', 'Connecting : ' . $username . ' ' . $password);
+        $bell->authenticate($username, $password);
+        print "My devices:\n";
+        log::add('ring', 'debug', 'Devices : ' . var_dump($bell->devices()));
+        while(1) {
+            $states = $bell->poll();
+            if ($states) {
+                foreach($states as $state) {
+                    if ($state['is_ding']) {
+                        log::add('ring', 'debug', 'Ring : ' . var_dump($state));
+                    }
 
-  function recording($id) {
-    $result = array();
-    $data = array();
-    $data['api_version'] = $this->_apiVersion;
-    $data['auth_token']  = $this->_authToken;
-    $response = $this->_httpCall('GET', $this->_urlTemplate($this->_urlRecording, array('id' => $id)), $data);
-    return $response;
-  }
-
-  function devices() {
-    $result = array();
-    $data = array();
-    $data['api_version'] = $this->_apiVersion;
-    $data['auth_token']  = $this->_authToken;
-    $response = $this->_httpCall('GET', $this->_urlDevices, $data);
-    return $response;
-  }
-
-  private function _arrayToUrlString($array) {
-    $string = '';
-    foreach($array as $k => $v) {
-      $string .= urlencode($k).'='.urlencode($v).'&';
+                    if ($state['is_motion']) {
+                        log::add('ring', 'debug', 'Motion : ' . var_dump($state));
+                    }
+                }
+            }
+            sleep(5);
+        }
     }
-    return substr($string,0, -1);
-  }
-
-  private function _urlTemplate($url, $data) {
-    foreach($data as $k => $v) {
-      $url = str_replace('{'.$k.'}', $v, $url);
-    }
-    return $url;
-  }
 
 }
 
 class ringCmd extends cmd {
-  public function execute($_options = null) {
-    if ($this->getType() == 'info') {
-      return;
-    }
+    public function execute($_options = null) {
+        if ($this->getType() == 'info') {
+            return;
+        }
 
-  }
+    }
 }
 
 ?>
